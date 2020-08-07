81c8c5255e8efec8304b16bb4f6fb0ac5108f3d8
fluxlog <fluxlog@hotmail.com>
2020-08-07 09:54:10 -0700

    ex1-3 passing tests

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,15 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
 
-    return None
+    for i in range(length):
+        cache[weights[i]] = i
+
+    for index, weight in enumerate(weights):
+        weight_list = limit - weight
+
+        if weight_list in cache:
+            i = cache[weight_list]
+
+            return (index, i) if index > i else (i, index)
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,14 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    source_ticket = {i.source:i for i in tickets}
 
+    route = []
+
+    this_ticket = source_ticket["NONE"]
+    while this_ticket.destination != "NONE":
+        route.append(this_ticket.destination)
+        this_ticket = source_ticket[this_ticket.destination]
+
+    route.append(this_ticket.destination)
     return route
